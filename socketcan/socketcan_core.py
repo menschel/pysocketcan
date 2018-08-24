a12992984e9e71edc9782f99184cac234449b414
Patrick Menschel <menschel.p@posteo.de>
2018-08-24 14:37:24 +0200

    regular commit

--- socketcan/socketcan_core.py
+++ socketcan/socketcan_core.py
@@ -2,7 +2,9 @@
 # author: (c) Menschel 2018
 # description: main file containing the structures etc
 #
-from ctypes import Structure,c_uint8,c_uint32,c_ulong
+from ctypes import Structure,c_uint8,c_uint32,c_uint64,c_long,sizeof
+# from enum import Enum
+
 #why this inconsistent use of long,intXX types
 
 #from include/uapi/linux/can/can.h
@@ -14,8 +16,10 @@ from ctypes import Structure,c_uint8,c_uint32,c_ulong
 #     __u8    __res1;  /* reserved / padding */
 #     __u8    data[CAN_MAX_DLEN] __attribute__((aligned(8)));
 # };
+CAN_EFF_FLAG = 0x80000000
 
 class can_frame(Structure):
+#     _pack_ = 1
     _fields_ = [
                 ('can_id',c_uint32),
                 ('can_dlc',c_uint8),
@@ -24,7 +28,14 @@ class can_frame(Structure):
                 ('__res1',c_uint8),
                 ('data',c_uint8 * 8),
                 ]
- 
+#     def __init__(self):
+#         self.can_id = 0
+#         self.can_dlc = 0
+#         self.__pad = 0
+#         self.__res0 = 0
+#         self.__res1 = 0
+#         for i in range(8):
+#             self.data[i] = 0
  
 # struct canfd_frame {
 #     canid_t can_id;  /* 32 bit CAN_ID + EFF/RTR/ERR flags */
@@ -36,6 +47,7 @@ class can_frame(Structure):
 # };
 CANFD_MAX_DLEN = 64
 class canfd_frame(Structure):
+#     _pack_ = 1
     _fields_ = [
                 ('can_id',c_uint32),
                 ('len',c_uint8),
@@ -52,6 +64,7 @@ class canfd_frame(Structure):
 # };
 
 class can_filter(Structure):
+#     _pack_ = 1
     _fields_ = [
                 ('can_id',c_uint32),
                 ('can_mask',c_uint32),
@@ -59,28 +72,41 @@ class can_filter(Structure):
                 
 #from include/uapi/linux/can/bcm.h
 
-struct bcm_timeval {
-	long tv_sec;
-	long tv_usec;
-};
+# struct bcm_timeval {
+# 	long tv_sec;
+# 	long tv_usec;
+# };
+# class bcm_timeval(Structure):
+#     _fields_ = [
+#                 ('tv_sec',c_long),
+#                 ('tv_usec',c_long),
+#                 ]
+
+    
 class bcm_timeval(Structure):
+#     _pack_ = 1
     _fields_ = [
-                ('tv_sec',c_long),
-                ('tv_usec',c_long),
+                ('tv_sec',c_uint64),
+                ('tv_usec',c_uint64),
                 ]
+    
+#     def __init__(self):
+#         self.tv_sec = 0
+#         self.tv_usec = 0
 
 
-struct bcm_msg_head {
-	__u32 opcode;
-	__u32 flags;
-	__u32 count;
-	struct bcm_timeval ival1, ival2;
-	canid_t can_id;
-	__u32 nframes;
-	struct can_frame frames[0];
-};
+# struct bcm_msg_head {
+# 	__u32 opcode;
+# 	__u32 flags;
+# 	__u32 count;
+# 	struct bcm_timeval ival1, ival2;
+# 	canid_t can_id;
+# 	__u32 nframes;
+# 	struct can_frame frames[0];
+# };
 
 class bcm_msg_head(Structure):
+#     _pack_ = 1
     _fields_ = [
                 ('opcode',c_uint32),
                 ('flags',c_uint32),
@@ -91,3 +117,35 @@ class bcm_msg_head(Structure):
                 ('nframes',c_uint32),
                 ('frames',can_frame),
                 ]
+    
+#     def __init__(self):
+#         self.opcode = 0
+#         self.flags = 0
+#         self.count = 0
+#         self.can_id = 0
+#         self.nframes = 0
+
+TX_SETUP = 1
+TX_DELETE = 2
+TX_READ = 3
+
+
+SETTIMER = 0x0001
+STARTTIMER = 0x0002
+# class opcode(Enum):stupid idea ;-Ps
+#     TX_SETUP = 1
+#     TX_DELETE = 2
+if __name__ == "__main__":
+#     head = bcm_msg_head()
+#     parts = (bytearray(head.opcode),
+#              bytearray(head.flags),
+#              bytearray(head.count),
+#              bytearray(head.ival1),
+#              bytearray(head.ival2),
+#              bytearray(head.can_id),
+#              bytearray(head.nframes),
+#              bytearray(head.frames)
+#              )
+#     
+#     [print(x) for x in parts]
+    print(sizeof(bcm_msg_head))
--- socketcan/testsocketcan.py
+++ socketcan/testsocketcan.py
@@ -3,19 +3,106 @@
 # description: quick and dirty tests socketcan interface via python3 sockets
 #
 import socket
-from .socketcan_core import *
+
+from socketcan_core import can_frame,bcm_msg_head,CAN_EFF_FLAG,TX_SETUP,TX_DELETE,SETTIMER,STARTTIMER,TX_READ
+import time
+from ctypes import sizeof
+#from array import array
 
 
-def test_can_raw():
+def test_can_raw_receive(interface="vcan0"):
+    #the basic steps to connect to a raw can socket
+    #first define the socket
     s = socket.socket(socket.AF_CAN,socket.SOCK_RAW,socket.CAN_RAW)
-    s.bind(("vcan0",))
+    #bind it
+    s.bind((interface,))
     
-    #msg = s.recvmsg(16)
+    #read a can_frame from the socket, that is 16bytes long
+    msg = s.recv(sizeof(can_frame))
+    #use the python3 buffer interface to directly cast the bytes to the can_frame structure
+    can_msg = can_frame.from_buffer_copy(msg)
+    
+    #print the received can message in candump style
+    print("{0} {1:X} [{2}] {3}".format(interface,can_msg.can_id,len(can_msg.data)," ".join(["{0:02X}".format(x) for x in can_msg.data])))
+    s.close()
+    return
+    #now start this script / function
+    #and open two shells and
+    #type "candump vcan0" in the first
+    #type "cansend vcan0 01a#11223344AABBCCDD" in the second
+    #the output from python and from candump should look like this
+    #vcan0  01A   [8]  11 22 33 44 AA BB CC DD
+    
+def test_can_raw_transmit(interface="vcan0"):
+    #first define the socket
+    s = socket.socket(socket.AF_CAN,socket.SOCK_RAW,socket.CAN_RAW)
+    #bind it
+    s.bind((interface,))
+    
     msg = can_frame()
-    buffers = memoryview(msg)
-    s.recvmsg_into(buffers)
-    s.sendmsg([msg[0]],)
+    msg.can_id = 0x12345678
+    if msg.can_id > 0x7FF:
+        msg.can_id |= CAN_EFF_FLAG
+    data = [0,1,2,3,4,5,6,7]
+    for i,x in enumerate(data):
+        msg.data[i]=x
+    msg.can_dlc = len(data)
+    s.send(msg)
+    s.close()
+    return
+    
 
-import socket
-s = socket.socket(socket.PF_CAN,socket.SOCK_DGRAM,socket.CAN_BCM)
-s.connect(("vcan0",))
+
+def test_can_bcm_transmit(interface="vcan0"):#CHECK THIS WORKS NOW, so if it does not work tomorrow, there is something fishy
+    #first define the socket
+    s = socket.socket(socket.PF_CAN,socket.SOCK_DGRAM,socket.CAN_BCM)
+    #connect instead of bind presumably because the socket is of type SOCK_DGRAM
+    s.connect((interface,))
+    
+    msg = can_frame()
+    msg.can_id = 0x123
+    data = [0,1,2,3,4,5,6,7]
+    for i,x in enumerate(data):
+        msg.data[i]=x
+    msg.can_dlc = len(data)
+    
+    head = bcm_msg_head()
+    head.opcode = TX_SETUP
+    head.flags = (SETTIMER | STARTTIMER);
+    #print(head.flags)
+#     head.count = 0
+#     head.ival1.tv_sec = 0
+#     head.ival1.tv_usec = 0
+    head.ival2.tv_sec = 1
+    head.ival2.tv_usec = 0
+    head.can_id = msg.can_id
+    head.nframes = 1
+    head.frames=msg
+    
+    
+    s.send(head)
+    #a = bytearray(head)
+    #print(len(a))
+    
+#     head.opcode = TX_READ
+#     s.send(head)
+#     
+#     msg = s.recv(sizeof(bcm_msg_head)) 
+#     res = bcm_msg_head.from_buffer_copy(msg)
+    #print(res.opcode,res.flags)
+    
+    time.sleep(10)
+    head.opcode = TX_DELETE
+    s.send(head)
+    s.close()
+    return
+    
+
+
+# s = socket.socket(socket.PF_CAN,socket.SOCK_DGRAM,socket.CAN_BCM)
+# s.connect(("vcan0",))
+
+if __name__ == "__main__":
+    #test_can_raw_receive()
+    #test_can_raw_transmit()
+    test_can_bcm_transmit()
