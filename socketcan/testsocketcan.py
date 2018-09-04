e8817da9997e8b8c8d56e21b657c528e6349679c
Patrick Menschel <menschel.p@posteo.de>
2018-09-04 21:03:08 +0200

    isotp receive tested

--- socketcan/testsocketcan.py
+++ socketcan/testsocketcan.py
@@ -137,6 +137,21 @@ def test_isotp_transmit(interface="vcan0",rx_addr=0x7E0,tx_addr=0x7E8):
 # 00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F 10 11 12 13 14 15 16 17 18 19 1A 1B 1C 1D 1E 1F 20 21 22 23 24 25 26 27 28 29 2A 2B 2C 2D 2E 2F 30 31 32 33 34 35 36 37 38 39 3A 3B 3C 3D 3E 3F 
     return
 
+
+def test_isotp_receive(interface="vcan0",rx_addr=0x7E0,tx_addr=0x7E8):
+    #first define the socket
+    s = socket.socket(socket.AF_CAN,socket.SOCK_DGRAM,socket.CAN_ISOTP)
+    #bind it
+    s.bind((interface, rx_addr, tx_addr))
+    
+#    data = bytes(list(range(64)))
+
+    data = bytearray(s.recv(64))
+    print(data)
+    s.close()
+#echo 11 22 33 44 55 66 DE AD BE EF | isotpsend -s 7e0 -d 7e8 vcan0
+    return
+
 def test_can_bcm_receive(interface="vcan0"):
     import threading
     worker = threading.Thread(target=lambda : test_can_bcm_transmit(interface=interface))
@@ -182,4 +197,5 @@ if __name__ == "__main__":
 #     test_can_raw_transmit()
 #     test_can_bcm_transmit()
 #     test_can_fd_raw_transmit()
-    test_isotp_transmit()
+#     test_isotp_transmit()
+    test_isotp_receive()
