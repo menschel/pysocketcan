11e3c84bac05bc743af9d862be5d94135e53c091
Patrick Menschel <menschel.p@posteo.de>
2021-02-14 13:45:22 +0100

    Add flake8 check and apply some pep8 fixes

--- setup.py
+++ setup.py
@@ -14,17 +14,15 @@ setuptools.setup(
     url="https://github.com/menschel/socketcan",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Development Status :: 2 - Beta",
-        "Programming Language :: Python :: 3",
-        "License :: CC-BY-NC",
-        "Operating System :: OS Independent",
-        'Intended Audience :: Science/Research',
-        'Intended Audience :: Developers',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Software Development :: Embedded Systems',
-        'Topic :: Utilities',
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: POSIX :: Linux",
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Software Development :: Embedded Systems",
     ],
-    python_requires='>=3.7',
-    keywords='socketcan can'
+    python_requires=">=3.7",
+    keywords="socketcan can"
 
 )
